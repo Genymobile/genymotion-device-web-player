0baca98cf2db26259ff02ac784c807d0220ca620
Gilles Lepretre <glepretre@genymobile.com>
2022-09-09 19:17:38 +0200

    [WIP] Webpack JS build

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const path = require('path');
+
+module.exports = {
+    // entry: {index: path.resolve(__dirname, 'src', 'GenymotionManager.js')},
+    entry: ['./src/GenymotionManager.js', './src/worker/FileUploaderWorker.js'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+};

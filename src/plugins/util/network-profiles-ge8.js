d79284c0663b2a42458595f69747b6e6e347e878
mchatti <mchatti@genymobile.com>
2022-12-02 09:02:34 +0100

    Add network profile list and signal strength list files

---
+++ src/plugins/util/network-profiles-ge8.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * List of available network profiles to send to the VM 
+ * for android versions greater or equal to 8
+ */
+module.exports = [{
+    id: 8,
+    name: 'wifi',
+    label: 'Wifi',
+}, {
+    id: 7,
+    name: '5G',
+    label: '5G',
+}, {
+    id: 6,
+    name: 'lte',
+    label: '4G',
+}, {
+    id: 5,
+    name: 'hsdpa',
+    label: '3G+',
+}, {
+    id: 4,
+    name: 'umts',
+    label: '3G',
+}, {
+    id: 3,
+    name: 'edge',
+    label: 'EDGE',
+}, {
+    id: 2,
+    name: 'gprs',
+    label: 'GPRS',
+}, {
+    id: 1,
+    name: 'gsm',
+    label: 'GSM',
+}, {
+    id: 0,
+    name: 'none',
+    label: 'None',
+}];
